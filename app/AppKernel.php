23d37393203011ee9b7bc4a3eb419bb87c5ee0e4
Benjamin Thuillier <benjaminthuillier@gmail.com>
2011-11-04 17:15:27 +0100

    add fosuserbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Symfony\Bundle\DoctrineMongoDBBundle\DoctrineMongoDBBundle(),
             new Bthuillier\Bundle\MainBundle\BthuillierMainBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
