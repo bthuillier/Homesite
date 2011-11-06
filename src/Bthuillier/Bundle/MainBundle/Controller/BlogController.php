8217a7afaa0f616f080bf6329661a59849f90ccc
Benjamin Thuillier <benjaminthuillier@gmail.com>
2011-11-06 21:54:43 +0100

    add blog edit action

--- src/Bthuillier/Bundle/MainBundle/Controller/BlogController.php
+++ src/Bthuillier/Bundle/MainBundle/Controller/BlogController.php
@@ -47,29 +47,54 @@ class BlogController extends ContainerAware {
         }
         return array("form" => $form->createView());
     }
-    
+
     /**
      * @Route("/list")
      * @Template()
      */
     public function listAction() {
-        $dm    = $this->container->get('doctrine.odm.mongodb.document_manager');
+        $dm = $this->container->get('doctrine.odm.mongodb.document_manager');
         $blogs = $dm->getRepository('BthuillierMainBundle:Blog')->findAll();
-        
+
         return array("blogs" => $blogs);
     }
-    
+
     /**
      * @Route("/{slug}/delete")
      * @Template()
      */
     public function deleteAction($slug) {
         $dm = $this->container->get('doctrine.odm.mongodb.document_manager');
-        $blog = $dm->getRepository('BthuillierMainBundle:Blog')->findOneBy(array('slug'=>$slug));
+        $blog = $dm->getRepository('BthuillierMainBundle:Blog')->findOneBy(array('slug' => $slug));
         $dm->remove($blog);
         $dm->flush();
         $url = $this->container->get('router')->generate('bthuillier_main_blog_list');
         return new RedirectResponse($url);
+    }
+
+    /**
+     * @Route("/{slug}/edit")
+     * @Template
+     */
+    public function editAction($slug) {
+        
+        $dm = $this->container->get('doctrine.odm.mongodb.document_manager');
+        $blog = $dm->getRepository('BthuillierMainBundle:Blog')->findOneBy(array('slug' => $slug));
+        $factory = $this->container->get('form.factory');
+        $form = $factory->create(new BlogType());
+        $form->setData($blog);
+        $request = $this->container->get('request');
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                $dm = $this->container->get('doctrine.odm.mongodb.document_manager');
+                $dm->persist($blog);
+                $dm->flush();
+                $url = $this->container->get('router')->generate('bthuillier_main_blog_list');
+                return new RedirectResponse($url);
+            }
+        }
+        return array("form" => $form->createView(), "blog" => $blog);
     }
 
 }
