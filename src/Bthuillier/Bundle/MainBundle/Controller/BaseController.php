9b89cf6e58e41bfddba39429868049a8f11c17d8
Benjamin Thuillier <benjaminthuillier@gmail.com>
2011-12-10 18:04:48 +0100

    add a basecontroller for some shortcuts

---
+++ src/Bthuillier/Bundle/MainBundle/Controller/BaseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bthuillier\Bundle\MainBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+/**
+ * Description of Controller
+ *
+ * @author bthuillier
+ */
+class BaseController extends ContainerAware {
+    
+    protected function get($id) {
+        return $this->container->get($id);
+    }
+    
+    /**
+     * 
+     * @return Symfony\Bundle\DoctrineMongoDBBundle\ManagerRegistry
+     */
+    protected function getDoctrine() {
+        return $this->container->get('doctrine.odm.mongodb');
+    }
+    
+
+    /**
+     * @return Doctrine\ODM\MongoDB\DocumentManager
+     */
+    protected function getManager() {
+        return $this->getDoctrine()->getManager();
+    }
+    
+    /**
+     * @param string $documentName  The name of the Document.
+     * 
+     * @return \Doctrine\ODM\MongoDB\DocumentRepository
+     */
+    protected function getRepository($documentName) {
+        return $this->getDoctrine()->getRepository($documentName);
+    }
+}
