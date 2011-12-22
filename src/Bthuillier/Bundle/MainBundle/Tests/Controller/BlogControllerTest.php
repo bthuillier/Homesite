6d82f93fe36e684273d67081f7b51201d5de759e
Benjamin Thuillier <benjaminthuillier@gmail.com>
2011-12-22 16:58:27 +0100

    update the test for the blog controller

--- src/Bthuillier/Bundle/MainBundle/Tests/Controller/BlogControllerTest.php
+++ src/Bthuillier/Bundle/MainBundle/Tests/Controller/BlogControllerTest.php
@@ -22,6 +22,25 @@ class BlogControllerTest extends WebTestCase {
         $crawler = $client->request('GET', '/blog/');
 
         $this->assertTrue($crawler->filter('article')->count() == 5);
-        $this->assertTrue($crawler->filter('#active a:contains("Blog")')->count() > 0);
+        $this->assertTrue($crawler->filter('.active a:contains("Blog")')->count() > 0);
+        $this->assertEquals($crawler->filter('.success:contains("Nouveau")')->count(), 0, "No add button");
+        $this->assertEquals($crawler->filter('.primary:contains("Editer")')->count(), 0, "No edit button");
     }
+    
+    public function testNew() {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/blog/list');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isRedirect('http://localhost/login'));        
+    }
+    
+    public function testList() {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/blog/new');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isRedirect('http://localhost/login'));          
+    }    
+    
 }
