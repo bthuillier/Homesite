038d52ae3a3a4832f0665f0a5382867bc2b0df8a
Benjamin Thuillier <benjaminthuillier@gmail.com>
2012-01-30 23:30:51 +0100

    add a mongo controller

---
+++ src/Bthuillier/Bundle/MainBundle/Controller/MongoController.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Homesite project.
+ *
+ * (c) Benjamin Thuillier <http://blog.bthuillier.fr/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bthuillier\Bundle\MainBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+/**
+ * Description of MongoController
+ *
+ * @author Benjamin Thuillier <benjaminthuillier@gmail.com>
+ */
+class MongoController extends BaseController{
+    
+    /**
+     * @return Doctrine\ODM\MongoDB\DocumentManager
+     */
+    protected function getDocumentManager() {
+        return $this->getDoctrine()->getManager();
+    }
+    
+    /**
+     * @param string $documentName  The name of the Document.
+     * 
+     * @return \Doctrine\ODM\MongoDB\DocumentRepository
+     */
+    protected function getRepository($documentName) {
+        return $this->getDoctrine()->getRepository($documentName);
+    }    
+}
